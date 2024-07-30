23e0baa2d73f38c35de9517559e05260d0fc7a8b
Salitha Gayan Wijewardhana <127938205+BlackFarrow@users.noreply.github.com>
2024-07-30 19:13:28 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
   //login back end eka hadaddi
-  const url = "http://localhost:4000";
+  const url = "https://food-delivery-app-ek0l.onrender.com";  //http://localhost:4000
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
