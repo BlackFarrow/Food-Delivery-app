c62c34d4cd6ee79227466f32ab5c24b0903169fc
Salitha Gayan Wijewardhana <127938205+BlackFarrow@users.noreply.github.com>
2024-10-22 11:48:09 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,   // This makes Vite listen on all IP addresses (0.0.0.0)
+    port: 3000,   // Use any port, like 3000
+  },
+});
